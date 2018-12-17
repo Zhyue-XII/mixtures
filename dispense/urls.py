1a8b074b0150f041c38ae0072ca9e61f14fdf366
zhyue <1551985018@qq.com>
2018-12-17 09:52:42 +0800

    [pad] 领料

--- dispense/urls.py
+++ dispense/urls.py
@@ -3,4 +3,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='dispense/index.html')),
+    path('allot', TemplateView.as_view(template_name='dispense/allot.html')),
+    path('weight', TemplateView.as_view(template_name='dispense/weight.html')),
 ]
