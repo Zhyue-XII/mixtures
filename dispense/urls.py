9790583eba170cdfdd326d3f054ebefb766ccd22
zhyue <1551985018@qq.com>
2018-12-17 14:59:35 +0800

    [pad] 退库

--- dispense/urls.py
+++ dispense/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='dispense/index.html')),
     path('allot', TemplateView.as_view(template_name='dispense/allot.html')),
     path('weight', TemplateView.as_view(template_name='dispense/weight.html')),
+    path('back', TemplateView.as_view(template_name='dispense/back.html')),
 ]
