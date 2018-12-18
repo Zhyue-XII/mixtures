82ded8a7defc56de99a9ac525e634b29434b9130
zhyue <1551985018@qq.com>
2018-12-18 18:33:55 +0800

    [pad] 配料1

--- batching/urls.py
+++ batching/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='batching/index.html')),
     path('addTask', TemplateView.as_view(template_name='batching/addTask.html')),
     path('select', TemplateView.as_view(template_name='batching/select.html')),
+    path('weight', TemplateView.as_view(template_name='batching/weight.html')),
+    path('shelling', TemplateView.as_view(template_name='batching/shelling.html')),   # 称重
+    path('recheck', TemplateView.as_view(template_name='batching/recheck.html')),  # 条码复核
+    path('reckon', TemplateView.as_view(template_name='batching/reckon.html')),   # 点袋
+    path('barcode', TemplateView.as_view(template_name='batching/barcode.html')),   # 条码点袋
 ]
